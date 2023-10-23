d5616af81f6cffb91bfef017db0e2db8ee3952b2
quocdat <datnq2003@gmail.com>
2023-10-23 15:00:07 +0700

    Chinh sua ten bang va diagram

--- DATABASE/ALOHCMUTE.sql
+++ DATABASE/ALOHCMUTE.sql
@@ -1,14 +1,14 @@
 create schema if not exists ALOHCMUTE;
 use ALOHCMUTE;
 
-create table tbAccount(
+create table `Account`(
 	gmail varchar(100) primary key,
     userName varchar(50) not null,
     password varchar(100) not null
 );
 
 -- Tạo bảng User 
-create table tbUser(
+create table `User`(
 	userID int primary key,
     gmail varchar(100),
 	mobile varchar(11),
@@ -23,57 +23,57 @@ create table tbUser(
     workPlace nvarchar(200),
     avatar text,
     cover text,
-    foreign key (gmail) references tbAccount(gmail)
+    foreign key (gmail) references `Account`(gmail)
 );
 
 -- Tạo bảng Group
-create table tbGroup(
+create table `Group`(
 	groupID int primary key,
     groupName nvarchar(100) not null,
     createTime datetime not null,
     createrID int,
-    foreign key (createrID) references tbUser(userID)
+    foreign key (createrID) references `User`(userID)
 );
 
 -- Tạo bảng Follow
-create table tbFollow(
+create table `Follow`(
 	sourceID int ,
     targetID int,
     fCreateTime datetime not null,
     fUpdateTime datetime not null,
     fStatus boolean not null default 1,
     primary key (sourceID, targetID),
-    foreign key (sourceID) references tbUser(userID),
-    foreign key (targetID) references tbUser(userID)
+    foreign key (sourceID) references `User`(userID),
+    foreign key (targetID) references `User`(userID)
 );
 
 -- Tạo bảng BoxChat
-create table tbBoxChat(
+create table `BoxChat`(
 	bcID int primary key,
     userID int,
-    foreign key (userID) references tbUser(userID)
+    foreign key (userID) references `User`(userID)
 );
 
 -- Tạo bảng Chat
-create table tbChat(
+create table `Chat`(
 	chatID int primary key,
     bcID int,
-    foreign key (bcID) references tbBoxChat(bcID)
+    foreign key (bcID) references `BoxChat`(bcID)
 ); 
 
 -- Tạo bảng UserPost
-create table tbUserPost(
+create table `UserPost`(
 	upID int primary key,
     userID int,
     upText nvarchar(5000) not null,
     upCreateTime datetime not null,
     upStatus boolean not null default 1,
     upUpdateTime datetime not null,
-	foreign key (userID) references tbUser(userID)
+	foreign key (userID) references `User`(userID)
 );
 
 -- Tạo bảng GroupPost
-create table tbGroupPost(
+create table `GroupPost`(
 	gpID int primary key,
     gpText nvarchar(5000) not null,
     gpCreateTime datetime not null,
@@ -82,28 +82,28 @@ create table tbGroupPost(
 );
 
 -- Tạo bảng GroupMember
-create table tbGroupMember(
+create table `GroupMember`(
 	groupID int,
     userID int,
     gpID int,
     primary key(groupID, userID, gpID),
-	foreign key (groupID) references tbGroup(groupID),
-	foreign key (userID) references tbUser(userID),
-    foreign key (gpID) references tbGroupPost(gpID)
+	foreign key (groupID) references `Group`(groupID),
+	foreign key (userID) references `User`(userID),
+    foreign key (gpID) references `GroupPost`(gpID)
 );
 
 -- Tạo bảng UserImage
-create table tbUserImage(
+create table `UserImage`(
 	uiID int primary key,
     upID int,
     uiFile text not null,
-    foreign key (upID) references tbUserPost(upID)
+    foreign key (upID) references `UserPost`(upID)
 );
 
 -- Tạo bảng GroupImage
-create table tbGroupImage(
+create table `GroupImage`(
 	giID int primary key,
     gpID int,
     giFile text not null,
-	foreign key (gpID) references tbGroupPost(gpID)
+	foreign key (gpID) references `GroupPost`(gpID)
 );
